3fcf45ae62c573785f9883704ae0621371dda578
James Pretorius <jamesmpretorius@gmail.com>
2023-12-18 23:31:50 -0500

    comment out keymap

--- init.lua
+++ init.lua
@@ -419,3 +419,9 @@ vim.keymap.set({ "n" }, "<leader>e", "", {
     noremap = true,
     desc = "Open explorer",
 })
+vim.keymap.set("n", "<leader>/", function()
+    require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+end, { desc = "Toggle comment line" })
+vim.keymap.set("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", {
+    desc = "Toggle comment for selection",
+})
