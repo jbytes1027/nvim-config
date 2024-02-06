fc66d758bfcb9a077de3cce1700c7b7f1964ae86
James Pretorius <jamesmpretorius@gmail.com>
2024-02-05 23:31:47 -0500

    Move vindent keybinding location

--- init.lua
+++ init.lua
@@ -30,22 +30,6 @@ vim.opt.shortmess:append("s") -- hide "search hit BOTTOM, continuing at TOP"
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
 vim.cmd("aunmenu PopUp.-1-")
 
--- Config vindent keybindings
-vim.g.vindent_motion_OO_prev = "[i" -- jump to prev block of same indent.
-vim.g.vindent_motion_OO_next = "]i" -- jump to next block of same indent.
-vim.g.vindent_motion_more_prev = "[+" -- jump to prev line with more indent.
-vim.g.vindent_motion_more_next = "]+" -- jump to next line with more indent.
-vim.g.vindent_motion_less_prev = "[-" -- jump to prev line with less indent.
-vim.g.vindent_motion_less_next = "]-" -- jump to next line with less indent.
-vim.g.vindent_motion_diff_prev = "[;" -- jump to prev line with different indent.
-vim.g.vindent_motion_diff_next = "];" -- jump to next line with different indent.
-vim.g.vindent_motion_XX_ss = "[p" -- jump to start of the current block scope.
-vim.g.vindent_motion_XX_se = "]p" -- jump to end   of the current block scope.
-vim.g.vindent_object_XX_ii = "ii" -- select current block.
-vim.g.vindent_object_XX_ai = "ai" -- select current block + one extra line  at beginning.
-vim.g.vindent_object_XX_aI = "aI" -- select current block + two extra lines at beginning and end.
-vim.g.vindent_jumps = 1 -- make vindent motion count as a |jump-motion| (works with |jumplist|).
-
 vim.g.diagnostics_hidden = true
 require("lsp").diagnostics_set_config()
 
@@ -148,6 +132,22 @@ require("lazy").setup({
     },
     {
         "jessekelighine/vindent.vim",
+        init = function()
+            vim.g.vindent_motion_OO_prev = "[i" -- jump to prev block of same indent.
+            vim.g.vindent_motion_OO_next = "]i" -- jump to next block of same indent.
+            vim.g.vindent_motion_more_prev = "[+" -- jump to prev line with more indent.
+            vim.g.vindent_motion_more_next = "]+" -- jump to next line with more indent.
+            vim.g.vindent_motion_less_prev = "[-" -- jump to prev line with less indent.
+            vim.g.vindent_motion_less_next = "]-" -- jump to next line with less indent.
+            vim.g.vindent_motion_diff_prev = "[;" -- jump to prev line with different indent.
+            vim.g.vindent_motion_diff_next = "];" -- jump to next line with different indent.
+            vim.g.vindent_motion_XX_ss = "[p" -- jump to start of the current block scope.
+            vim.g.vindent_motion_XX_se = "]p" -- jump to end   of the current block scope.
+            vim.g.vindent_object_XX_ii = "ii" -- select current block.
+            vim.g.vindent_object_XX_ai = "ai" -- select current block + one extra line  at beginning.
+            vim.g.vindent_object_XX_aI = "aI" -- select current block + two extra lines at beginning and end.
+            vim.g.vindent_jumps = 1 -- make vindent motion count as a |jump-motion| (works with |jumplist|).
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter",
