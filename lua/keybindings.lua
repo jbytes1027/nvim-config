6aac8a0c89721dbedc7b1d51fbbba4f4b61b6afa
James Pretorius <jamesmpretorius@gmail.com>
2024-03-19 08:05:36 -0400

    Improve open url

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -437,6 +437,33 @@ M.set_lowercase_marks_keybindings = function()
 end
 
 M.set_misc_keybindings = function()
+    local function open(uri)
+        local cmd
+        if vim.fn.has("win32") == 1 then
+            cmd = { "explorer", uri }
+        elseif vim.fn.has("macunix") == 1 then
+            cmd = { "open", uri }
+        else
+            if vim.fn.executable("xdg-open") == 1 then
+                cmd = { "xdg-open", uri }
+            elseif vim.fn.executable("wslview") == 1 then
+                cmd = { "wslview", uri }
+            else
+                cmd = { "open", uri }
+            end
+        end
+
+        local ret = vim.fn.jobstart(cmd, { detach = true })
+        if ret <= 0 then
+            local msg = {
+                "Failed to open uri",
+                ret,
+                vim.inspect(cmd),
+            }
+            vim.notify(table.concat(msg, "\n"), vim.log.levels.ERROR)
+        end
+    end
+
     local function open_explorer()
         if require("lazy.core.config").spec.plugins["ranger.nvim"] ~= nil then
             require("ranger-nvim").open(true)
@@ -515,11 +542,7 @@ M.set_misc_keybindings = function()
         vim.keymap.set({ "n", "x" }, "gx", function()
             local url = vim.fn.expand("<cfile>")
             if url ~= nil then
-                if vim.fn.has("macunix") == 1 then
-                    vim.cmd(('silent !open "%s"'):format(url))
-                else
-                    vim.cmd(('silent !xdg-open "%s"'):format(url))
-                end
+                open(url)
             else
                 print("No URL found in line.")
             end
