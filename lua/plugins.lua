11ed7fd561b1efd368ceafd741121cfc0a4da760
James Pretorius <jamesmpretorius@gmail.com>
2025-01-03 17:30:35 -0500

    Update inline markdown highlights

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,7 +76,7 @@ return {
             ---@diagnostic disable-next-line: missing-fields
             require("nvim-treesitter.configs").setup({
                 -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
@@ -102,7 +102,7 @@ return {
                     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
                     -- Using this option may slow down your editor, and you may see some duplicate highlights.
                     -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
+                    additional_vim_regex_highlighting = { "markdown" },
                 },
             })
         end,
