05824073e7dd8798512473fea42cadf04d11751d
James Pretorius <jamesmpretorius@gmail.com>
2025-01-07 11:54:47 -0500

    Disable auto fix end of file line

--- lua/options.lua
+++ lua/options.lua
@@ -33,6 +33,7 @@ vim.o.shortmess = "aoOtTIs"
 vim.o.smoothscroll = true
 vim.o.confirm = true
 vim.o.shada = ""
+vim.o.fixendofline = false
 
 -- remove the "How-to disable mouse" menu item and the separator above it: >vim
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
