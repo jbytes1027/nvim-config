047b0ab81278d13b76be181d703d6915b2f5557d
James Pretorius <git@jamespretorius.com>
2026-08-01 15:54:20 -0500

    Remove nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -195,50 +195,6 @@ return {
         event = "VeryLazy",
     },
     {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        enabled = vim.fn.executable("cc") == 1
-            or vim.fn.executable("gcc") == 1
-            or vim.fn.executable("clang") == 1
-            or vim.fn.executable("cl") == 1
-            or vim.fn.executable("zig") == 1,
-        config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require("nvim-treesitter.configs").setup({
-                -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown" },
-
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-
-                -- Automatically install missing parsers when entering buffer
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = true,
-
-                -- List of parsers to ignore installing (or "all")
-                ignore_install = { "diff", "html" },
-
-                highlight = {
-                    enable = true,
-
-                    -- Disable slow treesitter highlight for large files
-                    disable = function(_, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        ---@diagnostic disable-next-line: undefined-field
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then return true end
-                    end,
-
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = { "markdown" },
-                },
-            })
-        end,
-    },
-    {
         "folke/which-key.nvim",
         event = "VeryLazy",
         init = function()
