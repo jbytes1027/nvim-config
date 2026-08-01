0932741d2d1132a35688af9bdf5d3d006fbc7388
James Pretorius <git@jamespretorius.com>
2026-08-01 14:49:17 -0500

    Update audocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
     pattern = "*.min.*",
-    command = "TSDisable highlight | syntax off",
+    command = "syntax off",
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
